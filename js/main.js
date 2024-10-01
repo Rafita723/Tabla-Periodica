ba90fe1c4851fb9a54fadb0f9b1eba81fda9bd25
Rafita723 <fasitomj@gmail.com>
2024-10-01 20:30:49 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,204 @@
+const tabla = document.getElementById('tabla');
+
+const rubica = document.getElementById('rubidio')
+const potasio = document.getElementById('potasio')
+const sodio = document.getElementById('sodio')
+const litio =document.getElementById('litio')
+const hidrogeno = document .getElementById('hidrogeno');
+const cesio = document .getElementById('cesio')
+const francio = document .getElementById('francio')
+const berilio = document .getElementById('berilio')
+const magnesio = document .getElementById('magnesio')
+const calcio = document .getElementById('calcio')
+const estronicio = document .getElementById('estronicio')
+const bario = document .getElementById('bario')
+const radio = document .getElementById('radio')
+const lantanidos = document .getElementById('lantanidos')
+const actinidos = document .getElementById('actinidos')
+
+hidrogeno .addEventListener ('click', function(){
+    Swal.fire({
+        title: "1  Hidrogeno",
+        text: "2,1 +1 1 0,071 1s¹",
+        imageUrl: "./imagenes/Hexagonal.png.png",
+        imageHeight: 200,
+        icon: "info"
+      });
+})
+
+litio.addEventListener ('click', function(){
+    Swal.fire({
+        title: "3 Litio",
+        text: "1,0 1 2-1 0,53 [He] 2s¹",
+        imageUrl: "./imagenes/cubica.png",
+        imageHeight: 200,
+        icon: "info"
+      });
+})
+sodio.addEventListener ('click', function(){
+    Swal.fire({
+        title: "11 Sodio",
+        text: "0,9 1 2-8-1 0,97 [Ne]3s¹",
+        imageUrl: "./imagenes/Hexagonal.png.png",
+        imageHeight: 200,
+        icon: "info"
+      });
+})
+potasio.addEventListener ('click', function(){
+    Swal.fire({
+        title: "19 potasio",
+        text: "0,8 1 8-1 0,86 [Ar]4s¹",
+        imageUrl: "./imagenes/Hexagonal.png.png",
+        imageHeight: 200,
+        icon: "info"
+      });
+})
+rubidio.addEventListener ('click', function(){
+    Swal.fire({
+        title: "37 Rubidio",
+        text: "0,8 1 8-1 1,53 [Kr]5s¹",
+        imageUrl: "./imagenes/Hexagonal.png.png",
+        imageHeight: 200,
+        icon: "info"
+      });
+})
+cesio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "55 Cesio",
+      text: "0,7 1 18-8-1 1,90 [Xe]6s¹",
+      imageUrl: "./imagenes/Hexagonal.png.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+francio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "87 Francio",
+      text: "0,7 1 18-8-1  [Rn]7s¹",
+      imageUrl: "./imagenes/Hexagonal.png.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+berilio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "4 Berilio",
+      text: "1,5 2 2-2 1,85 [He]2s²",
+      imageUrl: "./imagenes/Hexagonal.png.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+magnesio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "12 Magnesio",
+      text: "1,2 2 2-8-2 1,74 [Ne]3s²",
+      imageUrl: "./imagenes/Hexagonal.png.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+calcio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "20 Calcio",
+      text: "1,0 2 8-2 1,55 [Ar] 4s²",
+      imageUrl: "./imagenes/Cara centrada.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+estronicio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "38 Estronicio",
+      text: "1,0 2 8-2 [Kr] 5s²",
+      imageUrl: "./imagenes/Cara centrada.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+bario.addEventListener ('click', function(){
+  Swal.fire({
+      title: "56 bario",
+      text: "0,9 2 18-8-2 3,5 [Xe] 6s²",
+      imageUrl: "./imagenes/cubica.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+radio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "88 Radio",
+      text: "0,9 2 18-8-2 5,0 [Rn] 7s²",
+      imageUrl: "./imagenes/cubica.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+escandio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "21 Escandio",
+      text: "1,3 3 9-2 3,0 [Ar] 3d¹ 4s²",
+      imageUrl: "./imagenes/cubica.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+itrio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "39 Itrio",
+      text: "1,3 3 9-2 4,47 [Kr] 4d¹ 5s²",
+      imageUrl: "./imagenes/cubica.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+titanio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "22 Titanio",
+      text: "1,5 4,3 10-2 4,51 [Ar] 3d¹ 4s²",
+      imageUrl: "./imagenes/Hexagonal.png.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+circonio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "40 Circonio",
+      text: "1,3 3 9-2 3,0 [Kr] 4d¹ 5s²",
+      imageUrl: "./imagenes/hexagonal.png.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+hafnio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "72 Hafnio",
+      text: "1,3 3 9-2 3,0 [Xe] 4f¹ 5d² 6s²",
+      imageUrl: "./imagenes/cubica.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+rutherfordio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "(261) Rutherfordio",
+      text: "32-10-2 -- [Rn] 5f¹ 6d² 7s²",
+      imageUrl: "./imagenes/cubica.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+lantanidos.addEventListener ('click', function(){
+  Swal.fire({
+      title: "57/71",
+      text: "Serie De Lantánidos",
+      
+    });
+})
+actinidos.addEventListener ('click', function(){
+  Swal.fire({
+      title: "89/103",
+      text: "Serie De Actínidos",
+      
+    });
+})
