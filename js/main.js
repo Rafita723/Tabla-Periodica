f3f3fc3b72bc1d2b0de6c628e7042f0549b241f3
Rafita723 <fasitomj@gmail.com>
2024-11-04 19:39:52 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -15,6 +15,29 @@ const bario = document .getElementById('bario')
 const radio = document .getElementById('radio')
 const lantanidos = document .getElementById('lantanidos')
 const actinidos = document .getElementById('actinidos')
+const circonio = document .getElementById('circonio')
+const hafnio = document .getElementById('hafnio')
+const rutherfordio = document .getElementById('rutherfordio')
+const escandio = document.getElementById('escandio')
+const rodio = document.getElementById ('rodio')
+const cromo = document.getElementById('cromo')
+const manganeso = document.getElementById('manganeso')
+const hierro = document.getElementById('hierro')
+const cobalto = document.getElementById('cobalto')
+const niobio = document.getElementById('niobio')
+const molibdeno = document.getElementById('molibdeno')
+const tecnecio = document.getElementById('tecnecio')
+const rutenio = document.getElementById('rutenio')
+const tantalio = document.getElementById('tantalio')
+const wolframio = document.getElementById('wolframio')
+const renio = document.getElementById('renio')
+const osmio = document.getElementById('osmio')
+const iridio = document.getElementById('iridio')
+const dubnio = document.getElementById('dubnio')
+const seaborgio = document.getElementById('seaborgio')
+const bohrio = document.getElementById('bohrio')
+const hassio = document.getElementById('hassio')
+const meitnerio = document.getElementById('meitnerio')
 
 hidrogeno .addEventListener ('click', function(){
     Swal.fire({
@@ -53,7 +76,7 @@ potasio.addEventListener ('click', function(){
         icon: "info"
       });
 })
-rubidio.addEventListener ('click', function(){
+rubica.addEventListener ('click', function(){
     Swal.fire({
         title: "37 Rubidio",
         text: "0,8 1 8-1 1,53 [Kr]5s¹",
@@ -200,5 +223,180 @@ actinidos.addEventListener ('click', function(){
       title: "89/103",
       text: "Serie De Actínidos",
       
+    });
+})
+vanadio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "23 vanadio",
+      text: "5-4-3-2 2-8-11-2 [Ar] 3d³ 4s²",
+      imageUrl: "./imagenes/cubica.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+rodio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "45 Rodio",
+      text: "12,4-2-3-4 2-8-18-16-1 [Kr] 4d⁸ 5s¹",
+      imageUrl: "./imagenes/cubica.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+cromo.addEventListener ('click', function(){
+  Swal.fire({
+      title: "24 Cromo",
+      text: "7,19-6-3-2 2-8-13-1 [Ar] 3d⁵ 4s¹",
+      imageUrl: "./imagenes/cubica.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+manganeso.addEventListener ('click', function(){
+  Swal.fire({
+      title: "25 Manganeso",
+      text: "7,43-7-6-4-2-3 2-8-13-2 [Ar] 3d⁵ 4s²",
+      imageUrl: "./imagenes/cubica.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+hierro.addEventListener ('click', function(){
+  Swal.fire({
+      title: "26 Hierro",
+      text: "7,86-2-3-2-8-14-2 [Ar] 3d⁶ 4s²",
+      imageUrl: "./imagenes/cubica.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+cobalto.addEventListener ('click', function(){
+  Swal.fire({
+      title: "27 Cobalto",
+      text: "9,9-2-3-2-8-15-2 [Ar] 3d⁷ 4s²",
+      imageUrl: "./imagenes/hexagonal.png.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+niobio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "41 Niobio",
+      text: "8,4-5-3-2-8-18-12-1 [Kr] 4d⁴ 5s¹",
+      imageUrl: "./imagenes/cubica.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+molibdeno.addEventListener ('click', function(){
+  Swal.fire({
+      title: "42 Molibdeno",
+      text: "10,2-6-5-4-3-2-2-8-18-13-1 [Kr] 4d⁵ 5s¹",
+      imageUrl: "./imagenes/cubica.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+tecnecio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "43 Tecnecio",
+      text: "11,5-7-4-2-8-18-14-1 [Kr] 4d⁶ 5s¹",
+      imageUrl: "./imagenes/hexagonal.png.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+rutenio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "44 Rutenio",
+      text: "12,2-2-3-4-6-8-2-8-18-15-1 [Kr] 4d⁷ 5s¹",
+      imageUrl: "./imagenes/hexagonal.png.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+tantalio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "73 Tantalio",
+      text: "16,6-5-2-8-18-32-11-2 [Xe] 4f¹⁴ 5d³ 6s²",
+      imageUrl: "./imagenes/cubica.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+wolframio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "74 Wolframio",
+      text: "19,3-6-5-4-3-2-2-8-18-32-12-2 [Xe] 4f¹⁴ 5d⁴ 6s²",
+      imageUrl: "./imagenes/cubica.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+renio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "75 Renio",
+      text: "21,0-7-6-4-2-(-1) 2-8-18-32-13-2 [Xe] 4f¹⁴ 5d⁵ 6s²",
+      imageUrl: "./imagenes/hexagonal.png.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+osmio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "76 Osmio",
+      text: "22,6-2-3-4-6-8 2-8-18-32-14-2 [Xe] 4f¹⁴ 5d⁶ 6s²",
+      imageUrl: "./imagenes/hexagonal.png.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+iridio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "77 Iridio",
+      text: "22,5-6-4-3-2 2-8-18-32-15-2 [Xe] 4f¹⁴ 5d⁷ 6s²",
+      imageUrl: "./imagenes/Cara centrada.png",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+dubnio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "105 Dubnio",
+      text: "2-8-18-32-32-11-2 [Rn] 5f¹⁴ 6d³ 7s²",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+seaborgio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "106 Seaborgio",
+      text: "2-8-18-32-32-13-2 [Rn] 5f¹⁴ 6d⁴ 7s²",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+bohrio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "107 Bohrio",
+      text: "2-8-18-32-32-13-2 [Rn] 5f¹⁴ 6d⁵ 7s²",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+hassio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "108 Hassio",
+      text: "2-8-18-32-32-14-2 [Rn] 5f¹⁴ 6d⁶ 7s²",
+      imageHeight: 200,
+      icon: "info"
+    });
+})
+meitnerio.addEventListener ('click', function(){
+  Swal.fire({
+      title: "109 Meitnerio",
+      text: "2-8-18-32-32-15-2 [Rn] 5f¹⁴ 6d⁷ 7s²",
+      imageHeight: 200,
+      icon: "info"
     });
 })
